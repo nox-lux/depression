5a25d8aa94ab29858c475a3e788d1d5aaff012a2
mono-drift <prattyushgoswami.coffee@gmail.com>
2025-12-26 21:36:42 +0530

    Added support for modern websites

--- scraper.py
+++ scraper.py
@@ -6,23 +6,40 @@
 #############################################################
 
 # import
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import time
 import requests
-from bs4 import BeautifulSoup
 
 # container
 def container():
     website = input('Enter a website to scrape:\n')
-    content = parser(website)
+    driver = browser()
+    content = parser(driver, website)
     output(content)
+    driver.quit()
 
-def parser(scrape):
-    response = requests.get(scrape)
-    soup = BeautifulSoup(response.text, "html.parser")
-    return soup
+def browser():
+    options = Options()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=options
+    )     
+    return driver
 
-def output(soup):
-    print(soup.prettify()[:1000])
-    print(soup.find_all("p"))
-        
+def parser(driver, url):
+    driver.get(url)
+    time.sleep(3)
+    return driver
+
+def output(driver):
+    required = driver.find_elements(By.TAG_NAME, "a")
+    print(f"Found {len(required)} <a> tags!")
+    for p in required:
+        print(p.text)
 # run
 container()
